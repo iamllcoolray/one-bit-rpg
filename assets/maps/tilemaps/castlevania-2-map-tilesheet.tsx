5e69cc0c9e59754390677b3c73692d75b034bedc
iamllcoolray <llcoolray24@gmail.com>
2024-08-07 22:19:24 -0400

    created the tilesheets for tile and a new tiled project to begin building the tiled maps

---
+++ assets/maps/tilemaps/castlevania-2-map-tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="castlevania-2-map-tilesheet" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="64" columns="8">
+ <image source="../tilesheets/castlevania-2-map-tilesheet.png" width="265" height="265"/>
+</tileset>
